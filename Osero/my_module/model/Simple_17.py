3c0c1aa7a4b6b5c0813e73d13ff163731b8ebb75
panda-nakami <kuippagure@gmail.com>
2024-05-31 18:06:01 +0900

    add model

---
+++ Osero/my_module/model/Simple_17.py
@@ -0,0 +1,50 @@
+from keras.models import Sequential
+from keras.layers import (
+    MaxPooling2D,
+    Flatten,
+    Dropout,
+    BatchNormalization,
+)
+
+from my_module.model import model_cmn
+from my_module.model.model_cmn import conv, dense, model_debug_disp
+
+MODEL_NAME = __name__
+
+INPUT_CHANNEL = model_cmn.INPUT_CHANNEL
+OSERO_HEIGHT = model_cmn.OSERO_HEIGHT
+OSERO_WIDTH = model_cmn.OSERO_WIDTH
+INPUT_SIZE = OSERO_HEIGHT * OSERO_WIDTH
+OUTPUT_SIZE = OSERO_HEIGHT * OSERO_WIDTH + 1
+
+
+def get_model(DEBUG=False):
+    # モデルの定義
+    model = Sequential()
+
+    conv_activation = "relu"
+
+    # 畳み込み層
+    model.add(conv(3, 128, conv_activation, True, True))
+    model.add(conv(3, 128, conv_activation))
+    model.add(conv(3, 128, conv_activation))
+    model.add(conv(3, 128, conv_activation))
+    model.add(conv(3, 128, conv_activation))
+    model.add(conv(3, 128, conv_activation))
+
+    model.add(Dropout(0.25))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+
+    # 全結合層
+    model.add(dense(256, "relu"))
+
+    # 出力層
+    model.add(dense(OUTPUT_SIZE, "softmax"))
+
+    # デバッグ
+    if DEBUG:
+        model_debug_disp(model)
+
+    return model
