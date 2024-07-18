3162d2ce2a39416d8d392c527fd37ffd8a1fd4cc
panda-nakami <kuippagure@gmail.com>
2024-07-18 11:52:02 +0900

    update

---
+++ OseroAlphaZero/calc_hash.py
@@ -0,0 +1,16 @@
+import hashlib
+
+
+# ハッシュ値を計算する関数
+def calculate_file_hash(filepath, hash_algo="sha256"):
+    hash_function = hashlib.new(hash_algo)
+    with open(filepath, "rb") as f:
+        while chunk := f.read(8192):
+            hash_function.update(chunk)
+    return hash_function.hexdigest()
+
+
+if __name__ == "__main__":
+    # ハッシュ値の計算
+    hash_value = calculate_file_hash("model/latest.h5")
+    print(hash_value)
